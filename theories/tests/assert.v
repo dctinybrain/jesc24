b6f7ce08934cc552bf10dfab107185cf5a3bcf60
David Swasey <swasey@mpi-sws.org>
2017-04-04 13:30:39 +0200

    Hoare triples for assert and assume.

---
+++ theories/tests/assert.v
@@ -0,0 +1,27 @@
+From iris.program_logic Require Import hoare.
+From iris.heap_lang Require Import notation.
+From iris.heap_lang.lib Require Import assume.
+From iris.heap_lang Require Import proofmode.
+
+(** * Hoare triples for assertions and assumptions *)
+
+Section ht.
+  Context `{heapG Σ}.
+  Implicit Types Φ : val → iProp Σ.
+
+  Lemma ht_assert p E e P Φ :
+    {{ P }} e @ p; E {{ v, ⌜v = #true⌝ ∧ ▷ Φ ()%V }} ⊢
+    {{ P }} assert: e @ p; E {{ Φ }}.
+  Proof.
+    iIntros "#He !# Hp". wp_apply wp_assert.
+    by iApply ("He" with "Hp").
+  Qed.
+
+  Lemma ht_assume e E P Φ :
+    {{ P }} e @ E ?{{ v, ⌜v = #true⌝ -∗ ▷ Φ ()%V }} ⊢
+    {{ P }} assume: e @ E ?{{ Φ }}.
+  Proof.
+    iIntros "#He !# Hp". wp_apply wp_assume.
+    by iApply ("He" with "Hp").
+  Qed.
+End ht.
