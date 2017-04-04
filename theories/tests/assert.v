e8eb39d71f5e518d87f87b225f34bb9dd78df20a
David Swasey <swasey@mpi-sws.org>
2017-04-04 13:38:17 +0200

    Tweak ht_assert.

--- theories/tests/assert.v
+++ theories/tests/assert.v
@@ -2,6 +2,7 @@ From iris.program_logic Require Import hoare.
 From iris.heap_lang Require Import notation.
 From iris.heap_lang.lib Require Import assume.
 From iris.heap_lang Require Import proofmode.
+Import uPred.
 
 (** * Hoare triples for assertions and assumptions *)
 
@@ -10,10 +11,12 @@ Section ht.
   Implicit Types Φ : val → iProp Σ.
 
   Lemma ht_assert p E e P Φ :
-    {{ P }} e @ p; E {{ v, ⌜v = #true⌝ ∧ ▷ Φ ()%V }} ⊢
+    {{ P }} e @ p; E {{ v, ⌜v = #true⌝ ∗ ▷ Φ ()%V }} ⊢
     {{ P }} assert: e @ p; E {{ Φ }}.
   Proof.
     iIntros "#He !# Hp". wp_apply wp_assert.
+    setoid_rewrite <- always_pure.
+    setoid_rewrite always_and_sep_l'.
     by iApply ("He" with "Hp").
   Qed.
 
