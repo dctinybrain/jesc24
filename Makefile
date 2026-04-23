7082c76bdbfa38905473faed74af7a4df3bcbdae
Dan Connolly <dckc@madmode.com>
2026-04-23 09:36:44 -0500

    jessie: generate parser sources from js files

--- Makefile
+++ Makefile
@@ -2,19 +2,34 @@
 
 COQ_MAKEFILE := coq_makefile
 REDIR := $(MAKE) -f Makefile.coq
+JS_TO_COQ := theories/jessie/tools/js_to_coq_source.py
+JS_SOURCES := theories/jessie/sources/makeCounter.js theories/jessie/sources/escrow2013.js
+GENERATED_JS_V := theories/jessie/makeCounter_js.v theories/jessie/escrow2013_js.v
 
-all: Makefile.coq
+all: $(GENERATED_JS_V) Makefile.coq
 	+@$(REDIR) $@
 .PHONY: all
 
-cleanall: Makefile.coq
-	+@$(REDIR) $@
+clean:
+	@if [ -f Makefile.coq ]; then $(REDIR) $@; fi
+	@echo CLEAN $(GENERATED_JS_V)
+	@rm -f $(GENERATED_JS_V)
+.PHONY: clean
+
+cleanall:
+	@if [ -f Makefile.coq ]; then $(REDIR) cleanall; fi
+	@echo CLEAN $(GENERATED_JS_V)
+	@rm -f $(GENERATED_JS_V)
 	@echo CLEAN Makefile.coq Makefile.coq.conf
 	@rm -f Makefile.coq Makefile.coq.conf
 .PHONY: cleanall
 
+$(GENERATED_JS_V): theories/jessie/%_js.v: theories/jessie/sources/%.js $(JS_TO_COQ)
+	@echo GEN $@
+	@python3 $(JS_TO_COQ) $< $@
+
 # Enable $(REDIR)
-Makefile.coq Makefile.coq.conf: _CoqProject Makefile
+Makefile.coq Makefile.coq.conf: _CoqProject Makefile $(GENERATED_JS_V)
 	+@echo COQ_MAKEFILE
 	+@$(COQ_MAKEFILE) -f _CoqProject -o Makefile.coq
 
@@ -23,4 +38,4 @@ Makefile.coq Makefile.coq.conf: _CoqProject Makefile
 	+@$(REDIR) $@
 
 # Don't $(REDIR)
-Makefile _CoqProject: ;
+Makefile _CoqProject $(JS_SOURCES) $(JS_TO_COQ): ;
--- theories/jessie/escrow2013.v
+++ theories/jessie/escrow2013.v
@@ -1,68 +1,11 @@
 From Coq Require Import List String ZArith.
 From iris.jessie.peg Require Import peg_match.
+From iris.jessie Require Import escrow2013_js.
 From iris.jessie Require Import escrow2013_target jessica_ast quasi_jessie.
 
 Import ListNotations.
 Open Scope string_scope.
 Open Scope Z_scope.
-
-Definition escrow2013_source : string :=
-  "import { E } from '@endo/far';
-
-const Q = Promise;
-
-const Qjoin = harden((p1, p2) =>
-  Q.all([p1, p2]).then(([r1, r2]) => {
-    if (!Object.is(r1, r2)) {
-      throw Error('join failed');
-    }
-    return r1;
-  })
-);
-
-const transfer = harden((decisionP, srcPurseP, dstPurseP, amount) => {
-  const makeEscrowPurseP = Qjoin(
-    E.get(srcPurseP).makePurse,
-    E.get(dstPurseP).makePurse
-  );
-  const escrowPurseP = E(makeEscrowPurseP)();
-  // setup phase 2
-  Q(decisionP).then(
-    _ => {
-      E(dstPurseP).deposit(amount, escrowPurseP);
-    },
-    _ => {
-      E(srcPurseP).deposit(amount, escrowPurseP);
-    }
-  );
-  return E(escrowPurseP).deposit(amount, srcPurseP); // phase 1
-});
-
-const failOnly = harden(cancellationP =>
-  Q(cancellationP).then(cancellation => {
-    throw cancellation;
-  })
-);
-
-// a from Alice , b from Bob
-const escrowExchange = harden((a, b) => {
-  let decide;
-  const decisionP = Q.promise(resolve => {
-    decide = resolve;
-  });
-  decide(
-    Q.race([
-      Q.all([
-        transfer(decisionP, a.moneySrcP, b.moneyDstP, b.moneyNeeded),
-        transfer(decisionP, b.stockSrcP, a.stockDstP, a.stockNeeded),
-      ]),
-      failOnly(a.cancellationP),
-      failOnly(b.cancellationP),
-    ])
-  );
-  return decisionP;
-});
-".
 
 Module Escrow2013.
   Import Escrow2013Target.
