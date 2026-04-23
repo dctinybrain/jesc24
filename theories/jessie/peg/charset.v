9d99ac6d025078b23becac7da814ecfb5b1015fe
Dan Connolly <dckc@madmode.com>
2026-04-23 01:27:49 -0500

    jessie: move parser and makeCounter proof line out of tests

---
+++ theories/jessie/peg/charset.v
@@ -0,0 +1,231 @@
+From Coq Require Import Bool.
+From Coq Require Import Strings.Ascii.
+From Coq Require Import Logic.FunctionalExtensionality.
+From iris.jessie.peg Require Import tactics.
+
+Definition charset : Type := (ascii -> bool).
+
+(* Empty charset *)
+Definition emptycharset : charset :=
+  (fun _ => false).
+
+(* Full charset *)
+Definition fullcharset : charset :=
+  (fun _ => true).
+
+(* Union of charsets *)
+Definition unioncharset cs1 cs2 : charset :=
+  (fun a => orb (cs1 a) (cs2 a)).
+
+Notation "cs1 '∪' cs2" := (unioncharset cs1 cs2) (at level 130, right associativity).
+
+(* Intersection of charsets *)
+Definition intersectioncharset cs1 cs2 : charset :=
+  (fun a => andb (cs1 a) (cs2 a)).
+
+Notation "cs1 '∩' cs2" := (intersectioncharset cs1 cs2) (at level 120, right associativity).
+
+(* Set complement of a charset *)
+Definition complementcharset cs : charset :=
+  (fun a => negb (cs a)).
+
+(* Disjoint charsets *)
+Definition disjointcharsets cs1 cs2 : Prop :=
+  (cs1 ∩ cs2) = emptycharset.
+
+Lemma unioncharset_diag :
+  forall cs, (cs ∪ cs) = cs.
+Proof.
+  intros.
+  extensionality a.
+  eauto using orb_diag.
+Qed.
+
+Lemma unioncharset_assoc :
+  forall cs1 cs2 cs3, (cs1 ∪ (cs2 ∪ cs3)) = ((cs1 ∪ cs2) ∪ cs3).
+Proof.
+  intros.
+  extensionality a.
+  eauto using orb_assoc.
+Qed.
+
+Lemma unioncharset_comm :
+  forall cs1 cs2, (cs1 ∪ cs2) = (cs2 ∪ cs1).
+Proof.
+  intros.
+  extensionality a.
+  eauto using orb_comm.
+Qed.
+
+Lemma unioncharset_rep_l :
+  forall cs1 cs2 cs3, ((cs1 ∪ cs3) ∪ (cs2 ∪ cs3)) = ((cs1 ∪ cs2) ∪ cs3).
+Proof.
+  intros.
+  extensionality a.
+  unfold unioncharset.
+  destruct (cs1 a);
+  destruct (cs2 a);
+  destruct (cs3 a);
+  eauto.
+Qed.
+
+Lemma unioncharset_fullcharset_l :
+  forall cs,
+  (fullcharset ∪ cs) = fullcharset.
+Proof.
+  intros.
+  extensionality a.
+  auto.
+Qed.
+
+Lemma unioncharset_emptycharset_l :
+  forall cs,
+  (emptycharset ∪ cs) = cs.
+Proof.
+  intros.
+  extensionality a.
+  auto.
+Qed.
+
+Lemma unioncharset_emptycharset_r :
+  forall cs,
+  (cs ∪ emptycharset) = cs.
+Proof.
+  intros.
+  extensionality a.
+  unfold emptycharset.
+  unfold unioncharset.
+  auto using orb_false_r.
+Qed.
+
+Lemma disjointcharsets_def :
+  forall cs1 cs2,
+  disjointcharsets cs1 cs2 ->
+  ~ exists a, cs1 a = true /\ cs2 a = true.
+Proof.
+  intros * H.
+  intros [a [H1 H2]].
+  unfold disjointcharsets in H.
+  apply equal_f with a in H.
+  unfold intersectioncharset in H.
+  unfold emptycharset in H.
+  rewrite H1 in H.
+  rewrite H2 in H.
+  discriminate.
+Qed.
+
+Lemma unioncharset_intersectioncharset_simpl :
+  forall cs1 cs2, (cs1 ∪ (cs1 ∩ cs2)) = cs1.
+Proof.
+  intros.
+  unfold unioncharset.
+  unfold intersectioncharset.
+  extensionality a.
+  destruct (cs1 a);
+  destruct (cs2 a);
+  auto.
+Qed.
+
+Inductive subcharseteq : charset -> charset -> Prop :=
+  | SubCharSetEq :
+      forall cs1 cs2,
+      subcharseteq cs1 (cs1 ∪ cs2)
+  .
+
+Notation "cs1 '⊆' cs2" := (subcharseteq cs1 cs2) (at level 120, right associativity).
+
+Lemma subcharseteq_refl :
+  forall cs,
+  cs ⊆ cs.
+Proof.
+  intros.
+  assert (cs ⊆ (cs ∪ cs))
+  as H by eauto using subcharseteq.
+  rewrite unioncharset_diag in H.
+  auto.
+Qed.
+
+Lemma subcharseteq_empty :
+  forall cs,
+  emptycharset ⊆ cs.
+Proof.
+  intros.
+  assert (emptycharset ⊆ (emptycharset ∪ cs))
+  as H by eauto using subcharseteq.
+  rewrite unioncharset_emptycharset_l in H.
+  auto.
+Qed.
+
+Lemma subcharseteq_trans :
+  forall cs1 cs2 cs3,
+  (cs1 ⊆ cs2) ->
+  (cs2 ⊆ cs3) ->
+  (cs1 ⊆ cs3).
+Proof.
+  intros * H12 H23.
+  inversion H12; subst.
+  inversion H23; subst.
+  rewrite <- unioncharset_assoc.
+  eauto using subcharseteq.
+Qed.
+
+Lemma subcharseteq_unioncharset :
+  forall cs1 cs2 cs3,
+  (cs1 ⊆ cs3) ->
+  (cs2 ⊆ cs3) ->
+  ((cs1 ∪ cs2) ⊆ cs3).
+Proof.
+  intros * H13 H23.
+  inversion H13; subst.
+  inversion H23; subst.
+  assert ((cs2 ∪ cs3) = ((cs1 ∪ cs2) ∪ (cs1 ∪ cs4)))
+  as Haux.
+  {
+    extensionality a.
+    unfold unioncharset.
+    unfold unioncharset in H1.
+    apply equal_f with a in H1.
+    destruct (cs1 a);
+    destruct (cs2 a);
+    destruct (cs3 a);
+    destruct (cs4 a);
+    auto.
+  }
+  rewrite Haux.
+  eauto using subcharseteq.
+Qed.
+
+Lemma subcharseteq_intersectioncharset_l :
+  forall cs1 cs2,
+  (cs1 ∩ cs2) ⊆ cs1.
+Proof.
+  intros.
+  assert (cs1 = ((cs1 ∩ cs2) ∪ cs1))
+  as Haux.
+  {
+    extensionality a.
+    unfold unioncharset.
+    unfold intersectioncharset.
+    destruct (cs1 a);
+    destruct (cs2 a);
+    auto.
+  }
+  remember (cs1 ∩ cs2) as cstemp.
+  rewrite Haux.
+  subst cstemp.
+  eauto using subcharseteq.
+Qed.
+
+Lemma subcharseteq_unioncharset_eq :
+  forall cs1 cs2,
+  (cs2 ⊆ cs1) ->
+  (cs1 ∪ cs2) = cs1.
+Proof.
+  intros * H.
+  inversion H; subst.
+  extensionality a.
+  unfold unioncharset.
+  destruct (cs2 a);
+  destruct (cs3 a);
+  auto.
+Qed.
---
+++ theories/jessie/quasi_justin.v
@@ -0,0 +1,89 @@
+From Coq Require Import Bool Lists.List Strings.Ascii Strings.String.
+From iris.jessie.peg Require Import syntax peg_match.
+From iris.jessie Require Import quasi_json.
+
+Import ListNotations.
+Open Scope string_scope.
+
+Module QuasiJustin.
+  Import QuasiJson.
+
+  (* Experimental peg-coq expression/recognizer layer, parallel to
+     quasi-justin, but limited to the fragments used by the current Jessie
+     makeCounter examples. This file defines PEG patterns and executable
+     recognition tests, not AST construction. *)
+
+  Definition ident_start : pat :=
+    charset_pat (fun a =>
+      orb (ascii_between 65 90 a)
+        (orb (ascii_between 97 122 a)
+          (Ascii.eqb a "_"%char))).
+
+  Definition ident_continue : pat :=
+    charset_pat (fun a =>
+      orb (ascii_between 65 90 a)
+        (orb (ascii_between 97 122 a)
+          (orb (ascii_between 48 57 a)
+            (Ascii.eqb a "_"%char)))).
+
+  Definition ident_core : pat := seq ident_start (star ident_continue).
+
+  (* quasi-justin.js.ts: useVar <- IDENT ${id => ['use', id]}; *)
+  Definition ident : pat := tok ident_core.
+
+  (* quasi-justin.js.ts:
+     memberPostOp <- LEFT_BRACKET indexExpr RIGHT_BRACKET / DOT IDENT_NAME / quasiExpr
+     callPostOp <- memberPostOp / args
+  *)
+  Definition post_op (expr_nt : nat) : pat :=
+    alt
+      (seq (sym ".") ident)
+      (seq (sym "(")
+        (seq
+          (opt (seq (PNT expr_nt) (star (seq (sym ",") (PNT expr_nt)))))
+          (sym ")"))).
+
+  (* quasi-justin.js.ts:
+     record <- LEFT_BRACE propDef ** _COMMA _COMMA? RIGHT_BRACE
+  *)
+  Definition object_pat (expr_nt propdef_nt : nat) : pat :=
+    seq (sym "{")
+      (seq
+        (opt
+          (seq (PNT propdef_nt)
+            (seq (star (seq (sym ",") (PNT propdef_nt)))
+              (opt (sym ",")))))
+        (sym "}")).
+
+  Definition grammar : grammar :=
+    [ (* 0 *) (* quasi-justin.js.ts: callExpr production subset. *)
+      seq (PNT 1) (star (post_op 0));
+      (* 1 *) (* quasi-justin.js.ts: primaryExpr production subset. *)
+      alt number
+                (alt (object_pat 0 2)
+                  (alt (seq (sym "(") (seq (PNT 0) (sym ")")))
+                       ident));
+      (* 2 *) (* quasi-justin.js.ts: propDef production subset. *)
+      seq (alt ident number) (seq (sym ":") (PNT 0))
+    ].
+
+  Definition expr : pat := PNT 0.
+  Definition primaryExpr : pat := PNT 1.
+  Definition propDef : pat := PNT 2.
+
+  Example parse_use_var :
+    matches_comp grammar expr "count" 128 = Some (Success EmptyString).
+  Proof. vm_compute. reflexivity. Qed.
+
+  Example parse_member_expr :
+    matches_comp grammar expr "c.incr" 128 = Some (Success EmptyString).
+  Proof. vm_compute. reflexivity. Qed.
+
+  Example parse_call_expr :
+    matches_comp grammar expr "makeCounter()" 256 = Some (Success EmptyString).
+  Proof. vm_compute. reflexivity. Qed.
+
+  Example parse_record_expr :
+    matches_comp grammar expr "{ incr: c.incr }" 512 = Some (Success EmptyString).
+  Proof. vm_compute. reflexivity. Qed.
+End QuasiJustin.
